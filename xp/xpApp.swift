c36bf020838e5bd85f46f2e4b8877127269658ee
BraysonWheeler <lbw98att@gmail.com>
2023-04-15 09:55:54 -0500

    Initial Commit

---
+++ xp/xpApp.swift
@@ -0,0 +1,17 @@
+//
+//  xpApp.swift
+//  xp
+//
+//  Created by Logan Wheeler on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct xpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
